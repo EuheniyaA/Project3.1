1c6aa391e65b0c3c1843c11aa4b6e68cfc1297d7
Evgeniya <bolbat_evgenia@mail.ru>
2021-10-10 00:27:53 +0300

    add files

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate (int cost) {
+
+        int amountOfRubles =  20;
+        int miles = cost / amountOfRubles;
+
+    return miles;
+
+    }
+}
